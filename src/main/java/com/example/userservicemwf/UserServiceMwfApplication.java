e2ce1e0a26ab42ead24b6fcc1ff9155a27323559
Sachin07patel <Mog@gmail.com>
2024-02-19 02:49:12 +0530

    OAuth2 code

--- src/main/java/com/example/userservicemwf/UserServiceMwfApplication.java
+++ src/main/java/com/example/userservicemwf/UserServiceMwfApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UserServiceMwfApplication {
 
     public static void main(String[] args) {
+        
         SpringApplication.run(UserServiceMwfApplication.class, args);
     }
 
--- src/test/java/com/example/userservicemwf/UserServiceMwfApplicationTests.java
+++ src/test/java/com/example/userservicemwf/UserServiceMwfApplicationTests.java
@@ -1,13 +1,45 @@
 package com.example.userservicemwf;
 
+import com.example.userservicemwf.security.repository.JpaRegisteredClientRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.test.annotation.Commit;
+
+import java.util.UUID;
 
 @SpringBootTest
 class UserServiceMwfApplicationTests {
+    @Autowired
+    private JpaRegisteredClientRepository jpaRegisteredClientRepository;
 
     @Test
     void contextLoads() {
     }
 
+//    @Test
+//    @Commit
+//    void stroreRegisteredClientIntoDB() {
+//        RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
+//                .clientId("oidc-client")
+//                .clientSecret("{noop}secret")
+//                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+//                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+//                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+//                .redirectUri("https://oauth.pstmn.io/v1/callback")
+//                .postLogoutRedirectUri("https://oauth.pstmn.io/v1/callback")
+//                .scope(OidcScopes.OPENID)
+//                .scope(OidcScopes.PROFILE)
+//                .scope("ADMIN")
+//                .scope("STUDENT")
+//                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+//                .build();
+//
+//                jpaRegisteredClientRepository.save(oidcClient);
+//    }
 }
